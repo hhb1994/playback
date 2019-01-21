a3c73b8fc34c9d37f751156bbd1f77c641cb4a33
hanhongbin <hanhb1994924@qq.com>
2019-01-21 16:58:04 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import VueVideoPlayer from "vue-video-player";
+import "video.js/dist/video-js.css";
+import "vue-video-player/src/custom-theme.css";
+import "videojs-contrib-hls";
+
+Vue.use(VueVideoPlayer);
 
 Vue.config.productionTip = false;
 
